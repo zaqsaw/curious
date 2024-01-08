7a4c32f18f11af16075573043a3409b111b5f83d
zxtas <117494428+zxtas@users.noreply.github.com>
2024-01-07 22:35:51 -0800

    Changed from listen to command events prefix should work

--- src/bot.py
+++ src/bot.py
@@ -13,16 +13,16 @@ client = discord.Client(command_prefix='.', intents=intents)
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-@client.event
-async def on_message(ctx):
-    if ctx.content == '.goon':
-        print(f'called by: { ctx.author }')
-        await ctx.channel.send("https://cdn.discordapp.com/attachments/1036455284874432515/1193332807674646578/c6d96599ceda4988b99b2d9fb75a64b6.mov?ex=65ac54c8&is=6599dfc8&hm=afe9862cd5311b9302254f92a8dca17fefb9f880c97e8b96777cec74bdbc0008&")
-    if ctx.content == '.cap':
+@client.command()
+async def goon(ctx):
+    await ctx.send("https://cdn.discordapp.com/attachments/1036455284874432515/1193332807674646578/c6d96599ceda4988b99b2d9fb75a64b6.mov?ex=65ac54c8&is=6599dfc8&hm=afe9862cd5311b9302254f92a8dca17fefb9f880c97e8b96777cec74bdbc0008&")
+
+@client.command()
+async def cap(ctx):
         print(f'called by: { ctx.author }')
         with open('genz_quotes.txt', 'r') as f:
             lines = list(f.readlines())
-        await ctx.channel.send(choice(lines))
+        await ctx.send(choice(lines))
 
 client.run(token)
 
