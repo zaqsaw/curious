d025ce3249656361e856014653693a9c28219be5
zxtas <117494428+zxtas@users.noreply.github.com>
2024-01-07 22:56:49 -0800

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -13,7 +13,7 @@ client = commands.Bot(command_prefix='.', intents=intents)
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-@client.command(aliases=["gook","chink")
+@client.command(aliases=["gook","chink") # reo was here :D
 async def goon(ctx):
     await ctx.send("https://cdn.discordapp.com/attachments/1036455284874432515/1193332807674646578/c6d96599ceda4988b99b2d9fb75a64b6.mov?ex=65ac54c8&is=6599dfc8&hm=afe9862cd5311b9302254f92a8dca17fefb9f880c97e8b96777cec74bdbc0008&")
 
