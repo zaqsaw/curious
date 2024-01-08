9b8445b127fc4aadc67288e29e702bd39e9748d4
zaqsaw <zaqsaw@gmail.com>
2024-01-07 21:58:04 -0800

    add function to call caption with random line

--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,5 @@
 import os
+from random import choice
 
 import discord
 from discord.ext import commands, tasks
@@ -18,7 +19,10 @@ async def on_message(ctx):
         print(f'called by: { ctx.author }')
         await ctx.channel.send("https://cdn.discordapp.com/attachments/1036455284874432515/1193332807674646578/c6d96599ceda4988b99b2d9fb75a64b6.mov?ex=65ac54c8&is=6599dfc8&hm=afe9862cd5311b9302254f92a8dca17fefb9f880c97e8b96777cec74bdbc0008&")
     if ctx.content == '.test':
-        await ctx.channel.send('tested')
+        with open('genz_quotes.txt', 'r') as f:
+            lines = list(f.readlines())
+            line = choice(lines)
+        await ctx.channel.send(f'.caption { line }')
 
 client.run(token)
 
