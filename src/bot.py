e9b7bc23d18014ee86f427f4c865cbe10b7a531f
zaqsaw <zaqsaw@gmail.com>
2024-01-15 15:03:02 -0800

    implement save and show for arbitrary images

--- src/bot.py
+++ src/bot.py
@@ -34,13 +34,45 @@ async def goon(ctx):
     logger.info(f'goon called by: { ctx.author }')
     await ctx.send("https://cdn.discordapp.com/attachments/1036455284874432515/1193332807674646578/c6d96599ceda4988b99b2d9fb75a64b6.mov?ex=65ac54c8&is=6599dfc8&hm=afe9862cd5311b9302254f92a8dca17fefb9f880c97e8b96777cec74bdbc0008&")
 
-@client.command()
-async def show(ctx):
-    logger.info(f'show called by: { ctx.author }')
+def load_show_map():
+    global show_file
+    global show_map
+    show_map = {}
+    if show_file.is_file():
+        with open(show_file, "r") as file:
+            loaded = yaml.safe_load(file)
+        if loaded:
+            show_map = loaded
+    return show_map
+
+def save_to_show_map(key, value):
+    global show_file
+    logger.info("saving to %s %s: %s", show_file, key, value)
+    show_map = load_show_map()
+    show_map[key] = value
+    with open(show_file, "w") as file:
+        yaml.dump(show_map, file)
 
 @client.command()
-async def save(ctx):
-    logger.info(f'save called by: { ctx.author }')
+async def show(ctx, *words):
+    phrase = ' '.join(words)
+    logger.info('show %s called by: %s', phrase, ctx.author)
+    show_map = load_show_map()
+    if phrase in show_map:
+        await ctx.send(show_map[phrase])
+
+@client.command()
+async def save(ctx, *words):
+    phrase = ' '.join(words)
+    logger.info('save %s called by: %s', phrase, ctx.author)
+    attachments = ctx.message.attachments
+    if phrase and len(attachments) == 1:
+        attachment = attachments[0]
+        url = attachment.url
+        save_to_show_map(phrase, url)
+        await ctx.send(f"saved { phrase }!")
+    else:
+        await ctx.send('".save phrase" requires a phrase and one attachment')
 
 @client.command(aliases=["genz","slang","hoodspeak"])
 async def cap(ctx):
@@ -69,5 +101,6 @@ if __name__ == "__main__":
         genz = yaml.safe_load(file)["quotes"]
     with open(args.config / "ious.yml", "r") as file:
         ious = yaml.safe_load(file)["statuses"]
+    show_file = args.config / "show.yml"
 
     client.run(token, log_handler=handler, log_level=logging.INFO)
